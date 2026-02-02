157f867a7d9a4930afd6c3266300c907ebc1fe14
ABdiallo1 <allassbdiallo.abd@gmail.com>
2026-02-02 00:20:45 +0000

    Affichage complet des colonnes dans l'admin

--- chauffeurs/admin.py
+++ chauffeurs/admin.py
@@ -3,8 +3,19 @@ from .models import Chauffeur
 
 @admin.register(Chauffeur)
 class ChauffeurAdmin(admin.ModelAdmin):
-    # On remplace 'nom' par 'nom_complet' dans la liste d'affichage
-    list_display = ('id', 'nom_complet', 'telephone', 'est_actif', 'date_expiration')
+    # Ajout de 'plaque_immatriculation' et 'est_en_ligne' dans la liste
+    list_display = ('id', 'nom_complet', 'telephone', 'plaque_immatriculation', 'est_actif', 'est_en_ligne', 'date_expiration')
+    
+    # Ajout de filtres pour trier rapidement sur le côté
     list_filter = ('est_actif', 'est_en_ligne')
-    search_fields = ('nom_complet', 'telephone')
-    ordering = ('-id',)
+    
+    # Champs de recherche
+    search_fields = ('nom_complet', 'telephone', 'plaque_immatriculation')
+    
+    # Organisation de la fiche détaillée (quand tu cliques sur un chauffeur)
+    fieldsets = (
+        ("Infos Personnelles", {'fields': ('nom_complet', 'telephone', 'plaque_immatriculation')}),
+        ("Documents", {'fields': ('photo_permis', 'photo_voiture')}),
+        ("Statuts & Abonnement", {'fields': ('est_actif', 'est_en_ligne', 'date_expiration')}),
+        ("Localisation", {'fields': ('latitude', 'longitude')}),
+    )
