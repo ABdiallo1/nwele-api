2fd686f07ef96f06c5ccda2ca37791bc43db757b
ABdiallo1 <allassbdiallo.abd@gmail.com>
2026-02-02 01:10:46 +0000

    Correction totale URLs et imports views

--- taxiprojet/urls.py
+++ taxiprojet/urls.py
@@ -1,13 +1,24 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from chauffeurs.views import (
+    creer_admin_force, 
+    ChauffeurListView, 
+    ChauffeurProfilView, 
+    PaiementChauffeurView, 
+    PaytechCallbackView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('chauffeurs.urls')),
+    
+    # Configuration
+    path('api/setup-admin/', creer_admin_force),
+    
+    # API Chauffeurs
+    path('api/chauffeurs/', ChauffeurListView.as_view()),
+    path('api/chauffeurs/<int:pk>/', ChauffeurProfilView.as_view()),
+    
+    # Paiement (C'est ce lien que tu utiliseras)
+    path('api/payer/<int:chauffeur_id>/', PaiementChauffeurView, name='payer_abonnement'),
+    path('api/paiement/callback/', PaytechCallbackView.as_view(), name='paytech_callback'),
 ]
-
-# Indispensable pour voir les photos des chauffeurs sur Render
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
