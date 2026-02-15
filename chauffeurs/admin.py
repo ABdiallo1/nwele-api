1bd3888489603b9c27076b48da2c9f299fb049e2
ABdiallo1 <allassbdiallo.abd@gmail.com>
2026-02-15 20:39:50 +0000

    Correction affichage admin

--- chauffeurs/admin.py
+++ chauffeurs/admin.py
@@ -4,43 +4,42 @@ from .models import Chauffeur
 
 @admin.register(Chauffeur)
 class ChauffeurAdmin(admin.ModelAdmin):
-    # Colonnes affichées dans la liste
+    # On définit ici EXACTEMENT les colonnes que l'on veut voir
     list_display = (
         'apercu_permis', 
-        'apercu_voiture', 
         'nom_complet', 
         'telephone', 
+        'plaque_immatriculation',  # <--- On l'ajoute explicitement ici
         'statut_abonnement', 
         'jours_restants_display',
-        'est_en_ligne'
+        'statut_service'
     )
     
-    # Filtres sur le côté droit
     list_filter = ('est_actif', 'est_en_ligne')
-    
-    # Barre de recherche
-    search_fields = ('nom_complet', 'telephone')
+    search_fields = ('nom_complet', 'telephone', 'plaque_immatriculation')
+
+    # --- Fonctions d'affichage personnalisées ---
 
     def apercu_permis(self, obj):
         if obj.photo_permis:
-            return format_html('<img src="{}" style="width: 50px; height: 50px; border-radius: 5px;" />', obj.photo_permis.url)
-        return "Pas de photo"
+            return format_html('<img src="{}" style="width: 40px; height: 40px; border-radius: 4px;" />', obj.photo_permis.url)
+        return "Non fourni"
     apercu_permis.short_description = 'Permis'
-
-    def apercu_voiture(self, obj):
-        if obj.photo_voiture:
-            return format_html('<img src="{}" style="width: 50px; height: 50px; border-radius: 5px;" />', obj.photo_voiture.url)
-        return "Pas de photo"
-    apercu_voiture.short_description = 'Voiture'
 
     def statut_abonnement(self, obj):
         if obj.est_actif:
             return format_html('<b style="color: green;">✔ ACTIF</b>')
-        return format_html('<b style="color: red;">✘ INACTIF</b>')
+        return format_html('<b style="color: red;">✘ EXPIRÉ</b>')
     statut_abonnement.short_description = 'Abonnement'
 
     def jours_restants_display(self, obj):
         jours = obj.jours_restants
-        color = "green" if jours > 5 else "red"
-        return format_html('<span style="color: {};">{} jours</span>', color, jours)
+        couleur = "green" if jours > 0 else "red"
+        return format_html('<span style="color: {}; font-weight: bold;">{} j</span>', couleur, jours)
     jours_restants_display.short_description = 'Reste'
+
+    def statut_service(self, obj):
+        if obj.est_en_ligne:
+            return format_html('<span style="background: green; color: white; padding: 3px 10px; border-radius: 10px;">EN LIGNE</span>')
+        return format_html('<span style="background: red; color: white; padding: 3px 10px; border-radius: 10px;">HORS LIGNE</span>')
+    statut_service.short_description = 'Service'
