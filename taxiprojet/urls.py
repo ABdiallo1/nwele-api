f33790d05ae7d92a689a0784a4bf108377ba7b8c
ABdiallo1 <allassbdiallo.abd@gmail.com>
2026-02-20 20:32:33 +0000

    Fix Pillow error and update templates

--- taxiprojet/urls.py
+++ taxiprojet/urls.py
@@ -1,7 +1,9 @@
+from django.contrib import admin # Ajoute cette ligne
 from django.urls import path
 from chauffeurs import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls), # REMETS CETTE LIGNE
     path('api/connexion-chauffeur/', views.connexion_chauffeur),
     path('api/initier-paiement/<int:chauffeur_id>/', views.initier_paiement),
     path('api/update-chauffeur/<int:chauffeur_id>/', views.update_chauffeur),
